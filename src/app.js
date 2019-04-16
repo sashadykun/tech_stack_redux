44656bab9701d76944ed5f5ac61e0064ac6727a6
SD <sashaluckyman@gmail.com>
2019-04-15 20:08:00 -0700

    init commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+
+const App = () => {
+    return (
+        <View style={{ padding: 60, alignItems: 'center' }}>
+            <Text>this app is in here</Text>
+        </View>
+    )
+} 
+
+export default App;
