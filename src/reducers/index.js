964ee262b0b23fea435a76912f5ea61c43836ea1
SD <sashaluckyman@gmail.com>
2019-04-17 00:07:21 -0700

    create reducer and json object

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
+import LibraryReducer from './library_reducer';
+
 
 export default combineReducers({
-    libraries: () => []
+    libraries: LibraryReducer
 })
