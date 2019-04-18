c45bc06351b922e993b312c00417afdd6208c306
SD <sashaluckyman@gmail.com>
2019-04-17 23:43:44 -0700

    render list of titles

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from './reducers'
-import { Header } from './components/common'
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/library_list';
+
 
 
 const App = () => {
     return (
         <Provider store={createStore(reducers)}>
             <Header headerText="Tech Stack" />
-            <View />
+            <LibraryList />
         </Provider>
     )
 } 
--- src/components/library_list.js
+++ src/components/library_list.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './list_item';
 
 
 
 class LibraryList extends Component {
+
+
+    renderItem(library) {
+        return <ListItem library={library} />
+    }
+
     render(){
-        return 
+        console.log ('props', this.props )
+        return (
+                <FlatList data={this.props.libraries} 
+                renderItem={this.renderItem}
+                keyExtractor={(library) =>library.id}
+            />
+        )
     }
 }
 
-export default connect( )(LibraryList);
+const mapStateToProps = state => {
+    console.log ('state', state )
+    return { libraries: state.libraries}
+};
+
+export default connect(mapStateToProps)(LibraryList);
